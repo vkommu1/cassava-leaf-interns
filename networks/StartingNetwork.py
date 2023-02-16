8a4e45284fd77109e4978c30bd06aa48f55aa23f
Pirjot <apirjot@gmail.com>
2023-02-15 18:46:40 -0800

    torchsummary addition

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -4,15 +4,21 @@ import torch.nn.functional as F
 
 # https://github.com/uclaacmai/leaf-us-alone
 
+import itertools
 
 class StartingNetwork(nn.Module):
-  def __init__(self):
+  # Input Shape is a tuple of size 3 ex: (1, 224, 224)
+  def __init__(self, input_shape=(1,224,224)):
     # Call nn.Module's constructor--don't forget this
     super().__init__()
 
     # Define layers
+    # For our very simple model, we just flatten the inputs into a 1D tensor
+    size = list(itertools.accumulate(input_shape, lambda x, y: x * y))[-1]
+    print(size)
+
     self.flatten = nn.Flatten()
-    self.fc1 = nn.Linear(28 * 28, 256)
+    self.fc1 = nn.Linear(size, 256)
     self.fc2 = nn.Linear(256, 128)
     self.fc3 = nn.Linear(128, 10)
 
@@ -52,16 +58,16 @@ class ConvNet(torch.nn.Module):
         #   - The filter size (called a kernel size in the documentation)
         #     Below, we specify 5, so our filters will be of size 5x5.
         #   - The amount of padding (default = 0)
-        self.conv1 = nn.Conv2d(1, 4, kernel_size=5, padding=2) # notice how we use padding to prevent dimension reduction
-
+        self.conv1 = nn.Conv2d(3, 4, kernel_size=5, padding=2) # notice how we use padding to prevent dimension reduction
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, padding=1)
         # Pooling layer takes two arguments
         #   - Filter size (in this case, 2x2)
         #   - Stride
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(8 * 7 * 7, 256)
+        self.fc1 = nn.Linear(240000, 256)
         self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc3 = nn.Linear(128, 5)
 
         # Original Code Here
         # self.flatten = nn.Flatten()
@@ -83,7 +89,8 @@ class ConvNet(torch.nn.Module):
         # (n, 8, 14, 14)
         x = self.pool(x)
         # (n, 8, 7, 7)
-        x = torch.reshape(x, (-1, 8 * 7 * 7))
+        x = torch.flatten(x, 1)
+        # x = torch.reshape(x, (-1, 8 * 7 * 7))
         # (n, 8 * 7 * 7)
         x = self.fc1(x)
         x = F.relu(x)
@@ -95,7 +102,14 @@ class ConvNet(torch.nn.Module):
         # (n, 10)
         return x
 
+StartingNetwork = ConvNet
+if (__name__ == "__main__"):
+  print("Starting Network Debug...")
 
+  import torchsummary
+
+  new_network = StartingNetwork()
+  torchsummary.summary(new_network, (3, 800, 600))
 
 
 """
